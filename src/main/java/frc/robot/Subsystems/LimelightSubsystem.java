b666b7369195cdb9849f5567358dd7d887f38ff2
Hriday-S <hsz713s9@gmail.com>
2023-05-15 21:41:36 -0400

    Shufleboard on new base

--- src/main/java/frc/robot/Subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/Subsystems/LimelightSubsystem.java
@@ -7,6 +7,14 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import edu.wpi.first.networktables.GenericEntry;
+
+
+ import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
 public class LimelightSubsystem extends SubsystemBase {
     NetworkTable networkTable;
 
@@ -33,6 +41,18 @@ public class LimelightSubsystem extends SubsystemBase {
     private double horizontalResolution;
     private double verticalResolution;
 
+    private final GenericEntry m_txEntry;
+    private final GenericEntry m_tyEntry;
+    private final GenericEntry m_taEntry;
+
+    private final GenericEntry m_RegressionDistanceEntry;
+    private final GenericEntry m_TrigDistanceEntry;
+    private final GenericEntry m_AvgDistaanceEntry;
+
+    private final GenericEntry m_OrbitalAngleEntry;
+    private final GenericEntry m_FoundTagEntry;
+
+
     public LimelightSubsystem(double pipelineId, boolean ledOn, boolean cameraMode, double limelightAngle,
             double limelightLensHeight, double goalHeightInches, double horizontalResolution,
             double verticalResolution) {
@@ -48,7 +68,41 @@ public class LimelightSubsystem extends SubsystemBase {
         this.verticalResolution = verticalResolution;
 
         networkTable = NetworkTableInstance.getDefault().getTable("limelight");
+
+        ShuffleboardTab tab = Shuffleboard.getTab("Limelight");
+
+         ShuffleboardLayout NetworkTables = tab.getLayout("NetworkTables", BuiltInLayouts.kList).withSize(2, 2).withPosition(0, 0);
+         m_txEntry = NetworkTables.add("tx(POV)", networkTable.getEntry("tx")).getEntry();
+         m_tyEntry = NetworkTables.add("ty",networkTable.getEntry("ty")).getEntry();
+         m_taEntry = NetworkTables.add("ta",networkTable.getEntry("ta")).getEntry();
+
+         ShuffleboardLayout Distances = tab.getLayout("Angles", BuiltInLayouts.kList).withSize(2, 2).withPosition(2, 0);
+         m_RegressionDistanceEntry = Distances.add("Regression Distance", 0).getEntry();
+         m_TrigDistanceEntry = Distances.add("Trig Distance", 0).getEntry();
+         m_AvgDistaanceEntry = Distances.add("Average Distance", 0).getEntry();
+         
+
+         ShuffleboardLayout Other = tab.getLayout("Other", BuiltInLayouts.kList).withSize(2, 2).withPosition(4, 0);
+         m_OrbitalAngleEntry = Other.add("Orbital Angle", 0).getEntry();
+         m_FoundTagEntry = Other.add("Found Tag?", false).getEntry();
     }
+
+    public void updateShuffleboard()
+    {
+        m_txEntry.setDouble(horizontalAngle);
+        m_tyEntry.setDouble(verticalAngle);
+        m_taEntry.setDouble(area);
+
+        m_RegressionDistanceEntry.setDouble(areaDistance);
+        m_TrigDistanceEntry.setDouble(trigDistance);
+        m_AvgDistaanceEntry.setDouble(avgDistance);
+     
+
+        m_OrbitalAngleEntry.setDouble(orbitalAngle);
+        m_FoundTagEntry.setBoolean(foundTagBool);
+
+    }
+
 
     @Override
     public void periodic() {
@@ -67,34 +121,26 @@ public class LimelightSubsystem extends SubsystemBase {
         verticalAngle = verticalEntry.getDouble(0.0);
         area = areaEntry.getDouble(0.0);
         foundTag = foundTagEntry.getDouble(0.0);
-        SmartDashboard.putNumber("Horizontal Angle", horizontalAngle);
-        SmartDashboard.putNumber("Vertical Angle", verticalAngle);
-        SmartDashboard.putNumber("Area", area);
 
         orbitalAngle = Math.toDegrees(Math.hypot(horizontalAngle, verticalAngle));
-        SmartDashboard.putNumber("Orbital Angle", orbitalAngle);
 
         foundTagBool = (foundTag != 0) ? true : false;
-        SmartDashboard.putBoolean("Tag Found?", foundTagBool);
 
         areaDistance = Units.inchesToMeters(54.4 * Math.pow(area, -0.475));
         /* TODO: Calculate trigonometric distances */
         trigDistance = Units.inchesToMeters(
                 (goalHeightInches - limelightLensHeight) / Math.tan(Math.toRadians(verticalAngle + limelightAngle)));
         avgDistance = (areaDistance + trigDistance) / 2;
-        SmartDashboard.putNumber("Area Distance", areaDistance);
 
         double horizontalDistance = Math.tan(Math.toRadians(horizontalAngle)) * areaDistance;
         double verticalDistance = Math.tan(Math.toRadians(verticalAngle)) * areaDistance;
-        SmartDashboard.putNumber("Horizontal Distance", horizontalDistance);
-        SmartDashboard.putNumber("Vertical Distance", verticalDistance);
+
 
         double cameraPixelX = horizontalAngle / 59.6 * horizontalResolution;
         double cameraPixelY = verticalAngle / 49.7 * verticalResolution;
         double cameraPixelArea = area / 100 * horizontalResolution * verticalResolution;
-        SmartDashboard.putNumber("Camera Pixel X", cameraPixelX);
-        SmartDashboard.putNumber("Camera Pixel Y", cameraPixelY);
-        SmartDashboard.putNumber("Camera Pixel Area", cameraPixelArea);
+
+        updateShuffleboard();
     }
     public double getHorizontalAngle() {
         return horizontalAngle;

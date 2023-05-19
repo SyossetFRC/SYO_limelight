dd4e75df4bd1fabd29f50e62fe36465681c37bd3
Hriday-S <hsz713x9@gmail.com>
2023-05-18 22:33:09 -0400

    command limelight winch elevator

--- src/main/java/frc/robot/Commands/TargetTrackingWithLimelight.java
+++ src/main/java/frc/robot/Commands/TargetTrackingWithLimelight.java
@@ -9,6 +9,8 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Subsystems.DrivetrainSubsystem;
 import frc.robot.Subsystems.LimelightSubsystem;
 
+
+/*A command that tracks april tags and moves towards them */
 public class TargetTrackingWithLimelight extends CommandBase {
     private final LimelightSubsystem m_limelightSubsystem;
     private final DrivetrainSubsystem m_drivetrainSubsystem;
@@ -29,17 +31,6 @@ public class TargetTrackingWithLimelight extends CommandBase {
 
     private ShuffleboardTab limelightTab;
 
-    /**
-     * @param drivetrainSubsystem The robot's drivetrain subsystem
-     * @param limelightSubsystem  The robot's Limelight subsystem
-     * @param distanceToPlayer    The desired distance to maintain from the player
-     * @param isFinishedTolerance The tolerance in which the command is considered
-     *                            finished
-     * @param kP                  The proportional constant for the PID controller
-     * @param deadband            The range in which small values will be ignored to
-     *                            avoid jitter
-     * @param fieldRelative       Whether to use field-relative control
-     */
     public TargetTrackingWithLimelight(DrivetrainSubsystem drivetrainSubsystem, LimelightSubsystem limelightSubsystem,
             double distanceToPlayer, double isFinishedTolerance, double kP, double deadband, boolean fieldRelative) {
         m_drivetrainSubsystem = drivetrainSubsystem;
@@ -54,8 +45,7 @@ public class TargetTrackingWithLimelight extends CommandBase {
         limelightTab = Shuffleboard.getTab("Limelight Tracking Command");
         ShuffleboardLayout limelightLayout = limelightTab.getLayout("Limelight Values", BuiltInLayouts.kList)
                 .withSize(2, 3).withPosition(0, 0);
-
-        limelightLayout.addNumber("Horizontal Velocity", () -> m_horizontalVelocity);
+        
         limelightLayout.addNumber("Vertical Velocity", () -> m_verticalVelocity);
         limelightLayout.addNumber("Angular Velocity", () -> m_angularVelocity);
         limelightLayout.addNumber("IsFinished() Tolerance", () -> this.isFinishedTolerance);
@@ -64,21 +54,12 @@ public class TargetTrackingWithLimelight extends CommandBase {
         addRequirements(m_drivetrainSubsystem);
     }
 
-    /**
-     * Initialize the command and create PID controllers for horizontal, vertical,
-     * and angular movements.
-     */
     public void initialize() {
         m_horizontalPIDController = new PIDController(kP, 0, 0);
         m_verticalPIDController = new PIDController(kP, 0, 0);
         m_angController = new PIDController(kP, 0, 0);
     }
 
-    /**
-     * Execute the command. Calculate velocities using PID controllers based on the
-     * Limelight data, apply deadbands,
-     * and drive the drivetrain.
-     */
     public void execute() {
         if (fieldRelative) {
             m_horizontalVelocity = m_horizontalPIDController.calculate(m_limelightSubsystem.getHorizontalTargetAngle());
@@ -100,16 +81,9 @@ public class TargetTrackingWithLimelight extends CommandBase {
                 fieldRelative);
 
         limelightTab.addNumber("Limelight Distance", () -> m_limelightSubsystem.getDistance());
-        limelightTab.addNumber("Limelight Horizontal Target Angle",
-                () -> m_limelightSubsystem.getHorizontalTargetAngle());
+        limelightTab.addNumber("Limelight Horizontal Target Angle", () -> m_limelightSubsystem.getHorizontalTargetAngle());
         limelightTab.addBoolean("Is Finished", () -> isFinished());
     }
-
-    /**
-     * @return True if the command is considered finished (when the robot is within
-     *         a certain tolerance of the target),
-     *         false otherwise.
-     */
 
     public boolean isFinished() {
         if (Math.abs(m_limelightSubsystem.getHorizontalTargetAngle()) > isFinishedTolerance
@@ -119,11 +93,6 @@ public class TargetTrackingWithLimelight extends CommandBase {
         return true;
     }
 
-    /**
-     * @param interrupted If the command was interrupted or not
-     *                    Stops the drivetrain when the command ends, regardless of
-     *                    reason (successful completion or interruption).
-     */
     @Override
     public void end(boolean interrupted) {
         m_drivetrainSubsystem.drive(0, 0, 0, true);
